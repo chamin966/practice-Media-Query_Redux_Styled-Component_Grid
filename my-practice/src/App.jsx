76cf837eabae84fd693168cbcaa847844470f7e3
minchan <chamin966@naver.com>
2023-04-13 00:11:55 +0900

    Test: styled-component 내부에서 미디어쿼리 사용 테스트

--- my-practice/src/App.jsx
+++ my-practice/src/App.jsx
@@ -1,27 +1,36 @@
 import styled from 'styled-components';
 
+/*
+styled-component 사용 시 주의사항!
+1. 반드시 styled-component 함수명을 대문자로 시작해야 한다.
+2. 함수 내부에 styled-component 함수가 있으면 동적 생성 에러가 발생하므로
+styled-component함수는 사용 함수 밖에 전역으로 생성할 것.
+*/
+
+const StyledButton = styled.button`
+  background-color: red;
+`;
+
+const LargeButton = styled(StyledButton)`
+  font-size: 150px;
+  color: ${({ primary }) => {
+    return primary ? 'blue' : 'white';
+  }};
+`;
+
+const GridContainer = styled.div`
+  border: 10px solid green;
+  display: grid;
+  grid-template-columns: 300px 1fr;
+  div {
+    border: 5px solid pink;
+  }
+  @media (max-width: 800px) {
+    display: block;
+  }
+`;
+
 function App() {
-  //styled-component는 반드시 함수명을 대문자로 시작해야 한다!
-  const StyledButton = styled.button`
-    background-color: red;
-  `;
-
-  const LargeButton = styled(StyledButton)`
-    font-size: 150px;
-    color: ${({ primary }) => {
-      return primary ? 'blue' : 'white';
-    }};
-  `;
-
-  const GridContainer = styled.div`
-    border: 10px solid green;
-    display: grid;
-    grid-template-columns: 300px 1fr;
-    div {
-      border: 5px solid pink;
-    }
-  `;
-
   return (
     <div>
       <div>Hello!</div>
